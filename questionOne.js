473cf8c9e3f6c97c7a3b750bc2fad8b78a31d86a
Sharon Morato <sharonmorato1@gmail.com>
2018-06-12 22:12:55 -0400

    first

---
+++ questionOne.js
@@ -0,0 +1,15 @@
+function sortByString(s, t) {
+  let string = s.split("");
+  let sortBy = t.split("");
+  let result = [];
+  for (let i = 0; i < sortBy.length; i++) {
+    for (let j = 0; j < string.length; j++) {
+      if (sortBy[i] === string[j]) {
+        result.push(string[j])
+      }
+    }
+  }
+  return result.join("");
+}
+
+sortByString("weather", "therapyw");
